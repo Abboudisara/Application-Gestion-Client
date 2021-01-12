43f4b59329c47e4705c9d6d2d49cd228df043118
Abboudisara <57992532+Abboudisara@users.noreply.github.com>
2021-01-12 14:27:53 +0100

    procedure1

---
+++ Gestion-Client/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace Gestion_Client
+{
+    partial class list1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.data = new System.Windows.Forms.DataGridView();
+            this.Ajt = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.TextBox();
+            this.Nom = new System.Windows.Forms.TextBox();
+            this.ville = new System.Windows.Forms.TextBox();
+            this.prenom = new System.Windows.Forms.TextBox();
+            this.adresse = new System.Windows.Forms.TextBox();
+            this.cherche = new System.Windows.Forms.Button();
+            this.sup = new System.Windows.Forms.Button();
+            this.upd = new System.Windows.Forms.Button();
+            this.ressup = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.data)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // data
+            // 
+            this.data.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.data.BackgroundColor = System.Drawing.SystemColors.HotTrack;
+            this.data.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data.Location = new System.Drawing.Point(-5, 2);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(813, 307);
+            this.data.TabIndex = 4;
+            this.data.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.data_CellContentClick);
+            // 
+            // Ajt
+            // 
+            this.Ajt.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Ajt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ajt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Ajt.Location = new System.Drawing.Point(12, 350);
+            this.Ajt.Name = "Ajt";
+            this.Ajt.Size = new System.Drawing.Size(147, 29);
+            this.Ajt.TabIndex = 5;
+            this.Ajt.Text = "Ajouter";
+            this.Ajt.UseVisualStyleBackColor = false;
+            // 
+            // ID
+            // 
+            this.ID.Location = new System.Drawing.Point(25, 426);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(318, 20);
+            this.ID.TabIndex = 7;
+            this.ID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Nom
+            // 
+            this.Nom.Location = new System.Drawing.Point(25, 470);
+            this.Nom.Name = "Nom";
+            this.Nom.Size = new System.Drawing.Size(177, 20);
+            this.Nom.TabIndex = 8;
+            // 
+            // ville
+            // 
+            this.ville.Location = new System.Drawing.Point(224, 533);
+            this.ville.Name = "ville";
+            this.ville.Size = new System.Drawing.Size(177, 20);
+            this.ville.TabIndex = 9;
+            // 
+            // prenom
+            // 
+            this.prenom.Location = new System.Drawing.Point(224, 469);
+            this.prenom.Name = "prenom";
+            this.prenom.Size = new System.Drawing.Size(177, 20);
+            this.prenom.TabIndex = 10;
+            // 
+            // adresse
+            // 
+            this.adresse.Location = new System.Drawing.Point(25, 533);
+            this.adresse.Name = "adresse";
+            this.adresse.Size = new System.Drawing.Size(177, 20);
+            this.adresse.TabIndex = 11;
+            // 
+            // cherche
+            // 
+            this.cherche.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.cherche.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cherche.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.cherche.Location = new System.Drawing.Point(437, 352);
+            this.cherche.Name = "cherche";
+            this.cherche.Size = new System.Drawing.Size(147, 29);
+            this.cherche.TabIndex = 12;
+            this.cherche.Text = "Recherche";
+            this.cherche.UseVisualStyleBackColor = false;
+            // 
+            // sup
+            // 
+            this.sup.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.sup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sup.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.sup.Location = new System.Drawing.Point(618, 348);
+            this.sup.Name = "sup";
+            this.sup.Size = new System.Drawing.Size(147, 29);
+            this.sup.TabIndex = 13;
+            this.sup.Text = "supprimer";
+            this.sup.UseVisualStyleBackColor = false;
+            this.sup.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // upd
+            // 
+            this.upd.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.upd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.upd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.upd.Location = new System.Drawing.Point(196, 350);
+            this.upd.Name = "upd";
+            this.upd.Size = new System.Drawing.Size(147, 29);
+            this.upd.TabIndex = 14;
+            this.upd.Text = "mise a jour";
+            this.upd.UseVisualStyleBackColor = false;
+            // 
+            // ressup
+            // 
+            this.ressup.Location = new System.Drawing.Point(437, 409);
+            this.ressup.Name = "ressup";
+            this.ressup.Size = new System.Drawing.Size(318, 20);
+            this.ressup.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(22, 412);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Identifiant";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(22, 454);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Nom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(231, 454);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "prenom";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(22, 517);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Adresse";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(227, 517);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "ville";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(440, 393);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Identifiant";
+            // 
+            // list1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(800, 582);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ressup);
+            this.Controls.Add(this.upd);
+            this.Controls.Add(this.sup);
+            this.Controls.Add(this.cherche);
+            this.Controls.Add(this.adresse);
+            this.Controls.Add(this.prenom);
+            this.Controls.Add(this.ville);
+            this.Controls.Add(this.Nom);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.Ajt);
+            this.Controls.Add(this.data);
+            this.Name = "list1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.list1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.data)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView data;
+        private System.Windows.Forms.Button Ajt;
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.TextBox Nom;
+        private System.Windows.Forms.TextBox ville;
+        private System.Windows.Forms.TextBox prenom;
+        private System.Windows.Forms.TextBox adresse;
+        private System.Windows.Forms.Button cherche;
+        private System.Windows.Forms.Button sup;
+        private System.Windows.Forms.Button upd;
+        private System.Windows.Forms.TextBox ressup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Gestion-Client/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Gestion_Client
+{
+    public partial class list1 : Form
+    {
+      
+        public list1()
+        {
+            InitializeComponent();
+           // connectionStrings
+        }
+        SqlConnection cnx = new SqlConnection(" Data Source=DESKTOP-IIVAD0V;Initial Catalog = GestionClient;Integrated Security=true");// connexion avec la base données
+        DataSet ds = new DataSet();// collection pour prendre un copie avant de l ajoute a un base données
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+         
+        private void list1_Load(object sender, EventArgs e)
+
+        {
+           // cnx.Open(); //ouverture d une base données
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM client", cnx); //pour données une commande 
+            adapter.Fill(ds, "client");// pour remplir une base données virtuelle travailler avec la methode fill(Nom bd virtuelle,Nom de table)
+            data.DataSource = ds.Tables["client"];// remplire le datagreedvue  avec le contenu de Datasource
+           // cnx.Close();// fermeture une base données
+        }
+
+        private void data_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
